e1171b2c0e57eecc7e001a160fdedeea4488b7ab
Sameer Jain <samir4nov@gmail.com>
2021-09-10 13:42:30 +0530

    App name renamed to QuoteDelight

--- android/app/src/main/java/com/samrintech/quotedelight/MainActivity.java
+++ android/app/src/main/java/com/samrintech/quotedelight/MainActivity.java
@@ -1,4 +1,4 @@
-package com.motivateme;
+package com.samrintech.quotedelight;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "MotivateMe";
+    return "QuoteDelight";
   }
 }
--- ios/QuoteDelightTests/QuoteDelightTests.m
+++ ios/QuoteDelightTests/QuoteDelightTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MotivateMeTests : XCTestCase
+@interface QuoteDelightTests : XCTestCase
 
 @end
 
-@implementation MotivateMeTests
+@implementation QuoteDelightTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
