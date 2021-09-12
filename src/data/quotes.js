5432bcdfe68a2720af680ef7f43cbcb650e63c73
Sameer Jain <samir4nov@gmail.com>
2021-09-12 23:58:04 +0530

    test data

---
+++ src/data/quotes.js
@@ -0,0 +1,24 @@
+export const QUOTES = [
+  {
+    id: 'q1',
+    quote: 'Tough times never last, but tough people do',
+    category: 'motivation',
+  },
+  {
+    id: 'q2',
+    quote: 'A crowded mind leaves no space for a peaceful heart',
+    category: 'inspiration',
+  },
+  {
+    id: 'q3',
+    quote:
+      'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that',
+    category: 'love',
+  },
+  {
+    id: 'q4',
+    quote:
+      'I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear',
+    category: 'inspiration',
+  },
+];
