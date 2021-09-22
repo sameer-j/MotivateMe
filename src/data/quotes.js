21ca08873f2c5db7dd60399fd25eb0cdeb1e7ea1
Sameer Jain <samir4nov@gmail.com>
2021-09-22 15:55:40 +0530

    test data update

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -21,4 +21,52 @@ export const QUOTES = [
       'I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear',
     category: 'inspiration',
   },
+  {
+    id: 'q5',
+    quote:
+      'Our starting point is always wrong. Instead of beginning with ourselves, we always want to change others first and ourselves last. If everyone would begin first with themselves, then there would be peace all around!',
+    category: 'cat1',
+  },
+  {
+    id: 'q6',
+    quote:
+      'There is nothing noble in being superior to your fellow man; true nobility is being superior to your former self',
+    category: 'cat2',
+  },
+  {
+    id: 'q7',
+    quote:
+      'She was powerful not because she wasn’t scared but because she went on so strongly, despite the fear',
+    category: 'cat3',
+  },
+  {
+    id: 'q8',
+    quote:
+      "Do actions agree with words? There's your measure of reliability. Never confine yourself to the words",
+    category: 'cat4',
+  },
+  {
+    id: 'q9',
+    quote:
+      'Whenever you are about to find fault with someone, ask yourself the following question: What fault of mine most nearly resembles the one I am about to criticize?',
+    category: 'cat5',
+  },
+  {
+    id: 'q10',
+    quote:
+      'The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well',
+    category: 'cat1',
+  },
+  {
+    id: 'q11',
+    quote:
+      'The mark of the immature man is that he wants to die nobly for a cause, while the mark of the mature man is that he wants to live humbly for one',
+    category: 'cat2',
+  },
+  {
+    id: 'q12',
+    quote:
+      'One Choice. One Choice, decides your friends. One Choice, defines your beliefs. One Choice, determines your loyalties - Forever. ONE CHOICE CAN TRANSFORM YOU',
+    category: 'cat3',
+  },
 ];
