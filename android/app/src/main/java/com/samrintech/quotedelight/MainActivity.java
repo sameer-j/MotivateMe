251b08bb331c93e28cb90a8d67f172016a882a05
Sameer Jain <samir4nov@gmail.com>
2021-09-29 18:50:17 +0530

    full screen view

--- android/app/src/main/java/com/samrintech/quotedelight/MainActivity.java
+++ android/app/src/main/java/com/samrintech/quotedelight/MainActivity.java
@@ -2,7 +2,9 @@ package com.samrintech.quotedelight;
 
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen; 
-import android.os.Bundle; 
+import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -18,5 +20,8 @@ public class MainActivity extends ReactActivity {
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this, R.style.SplashScreenTheme);
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
