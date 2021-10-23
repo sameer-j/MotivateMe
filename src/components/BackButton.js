8b87e8a4e7b9321b91a070198a0ce102e245ec0b
Sameer Jain <samir4nov@gmail.com>
2021-10-23 12:05:57 +0530

    Custom Back Navigation button

---
+++ src/components/BackButton.js
@@ -0,0 +1,17 @@
+import { useNavigation } from '@react-navigation/core';
+import React from 'react';
+import { TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+const BackButton = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={{ alignItems: 'flex-start' }}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={30} color="white" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BackButton;
