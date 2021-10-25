c8393a867418b039ce260e1de225a47c4c18e70f
Sameer Jain <samir4nov@gmail.com>
2021-10-25 20:54:01 +0530

    API: fetch data - v1

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+
 import Footer from './components/Footer';
 import QuoteCardCarousel from './components/QuoteCardCarousel';
 
-import { QUOTES } from '../../data/quotes';
+import { getParsedFirestoreJSON } from '../../util';
 
 function Home() {
+  const [isLoading, setLoading] = useState(true);
+  const [quotes, setQuotes] = useState([]);
+
+  const getQuotesFromApiAsync = async () => {
+    try {
+      const response = await fetch(
+        'https://firestore.googleapis.com/v1/projects/quote-delight-motivation/databases/(default)/documents/quotes',
+      );
+      const json = await response.json();
+      setQuotes(getParsedFirestoreJSON(json.documents));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getQuotesFromApiAsync();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <ActivityIndicator size={100} />
+      </View>
+    );
+  }
+
   return (
     <View style={{ flex: 1 }}>
-      <QuoteCardCarousel quotes={QUOTES} />
+      <QuoteCardCarousel quotes={quotes} />
       <View style={{ justifyContent: 'flex-end' }}>
         <Footer />
       </View>
