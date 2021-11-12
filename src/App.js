8f4c24af9bae31c785c41e084891b33e43010a96
Sameer Jain <samir4nov@gmail.com>
2021-11-12 17:13:38 +0530

    removed redux-persist

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import SplashScreen from 'react-native-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
 import { useColorScheme } from 'react-native';
 import { Provider } from 'react-redux';
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import Layout from './components/Layout';
 import Navigator from './navigator';
@@ -40,15 +38,13 @@ const App = () => {
   });
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
-        <NavigationContainer
-          linking={linking}
-          theme={systemTheme === 'dark' ? DarkTheme : LightTheme}>
-          <Layout>
-            <Navigator />
-          </Layout>
-        </NavigationContainer>
-      </PersistGate>
+      <NavigationContainer
+        linking={linking}
+        theme={systemTheme === 'dark' ? DarkTheme : LightTheme}>
+        <Layout>
+          <Navigator />
+        </Layout>
+      </NavigationContainer>
     </Provider>
   );
 };
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,16 +1,5 @@
 import { createStore } from 'redux';
-import AsyncStorage from '@react-native-community/async-storage';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-import { persistReducer } from 'redux-persist';
 
 import reducers from '../reducers';
 
-// persist config
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-  stateReconciler: autoMergeLevel2,
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
-export default createStore(persistedReducer);
+export default createStore(reducers);
