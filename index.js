7afef7e857d579100434573f3d40e8a2b6113025
sameer-j <samir4nov@gmail.com>
2021-11-13 12:35:14 +0530

    turning off console logs in prod

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
  * @format
  */
 
+// Turning off console logs in production environment
+console.log('Environment __DEV__ : ', __DEV__);
+if (!__DEV__) {
+  console.log = () => {};
+}
+
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
