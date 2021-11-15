1e076fa84734aae7663eca51de2dcdfee16c33e8
Sameer Jain <samir4nov@gmail.com>
2021-11-15 17:42:31 +0530

    updated upload script and data

--- data/firestoreQuotesUpload.js
+++ data/firestoreQuotesUpload.js
@@ -1,8 +1,8 @@
 const { getFirestore } = require('firebase-admin/firestore');
 const admin = require('firebase-admin');
-const QUOTES = require('./data/quotes');
+const QUOTES = require('./quotes');
 
-var serviceAccount = require('./secrets/serviceAccountKey.json');
+var serviceAccount = require('../secrets/secrets/serviceAccountKey.json');
 
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -17,7 +17,14 @@ const batch = db.batch();
 const uploadQuotes = async () => {
   for (let quote of quotes) {
     console.log(quote);
-    batch.set(db.collection('quotes').doc(quote.id), { ...quote });
+    const timestamp = Math.floor(Date.now() / 1000);
+    batch.set(db.collection('quotes').doc(quote.id), {
+      ...quote,
+      creationTime: timestamp,
+    });
+    batch.set(db.collection('quotesSize').doc('quotes'), {
+      count: quotes.length,
+    });
   }
   res = await batch.commit();
   console.log(res);
