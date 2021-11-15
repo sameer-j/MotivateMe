2063ca7c91c6b9e9992e77cff739615c11ced750
Sameer Jain <samir4nov@gmail.com>
2021-11-15 17:51:20 +0530

    settings page boilerplate setup

--- src/modules/settings/index.js
+++ src/modules/settings/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+import BackButton from '../../components/BackButton';
 
 const Settings = () => (
-  <View>
-    <Text>Settings Screen</Text>
+  <View style={styles.container}>
+    <View style={styles.topBar}>
+      <BackButton />
+      <Text>Settings Screen</Text>
+    </View>
   </View>
 );
 
+const styles = StyleSheet.create({
+  container: { marginTop: 70, flex: 1 },
+  topBar: {
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
 export default Settings;
